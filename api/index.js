e35fabbdfa9ca61f77abf6fa7c3ba4c3f9ee8b2d
teq-duyennguyen <duyennguyen@teqnological.asia>
2024-05-17 13:04:16 +0700

    Upload Code

---
+++ api/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import cookieParser from "cookie-parser";
+import multer from "multer";
+import { db } from "./db.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+db.connect((error) => {
+    if (error) {
+        console.error("Error connecting to MySQL database:", error);
+    } else {
+        console.log("Connected to MySQL database!");
+    }
+});
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "../client/public/upload");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname);
+    },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+    const file = req.file;
+    res.status(200).json(file.filename);
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+app.listen(8800, () => {
+    console.log("Connected!");
+});
